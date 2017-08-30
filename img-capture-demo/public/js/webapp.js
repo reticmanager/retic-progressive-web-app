34a86bc56a2344d003d2dcb8df563036f4022fb1
chaz.g <chazgoo@gmail.com>
2017-08-30 22:04:21 +1200

    bug fixes, template added, parent child emit event

--- img-capture-demo/public/js/webapp.js
+++ img-capture-demo/public/js/webapp.js
@@ -1,88 +1,119 @@
-
 new Vue({
-	el: '#app',
-	data: {
-		lat: "",
-		long: "",
-		date: "",
-		images: [],
-		image: "",
-		selected: "selected",
-		selected2: "selected",
-		rowId: 10,
-	},
-	methods: {
-		getLocation: function() {
-			if (navigator.geolocation) {
-				navigator.geolocation.getCurrentPosition(this.showPosition);
-			} else {
-				console.log('Geolocation not supported');
+  el: '#app',
+  data: {
+    lat: "",
+    long: "",
+    date: "",
+    images: [],
+    image: "",
+		childSelected: "",
+    manHole: false
+  },
+  components: {
+    'inspection-dropdown': {
+      template: '#inspection',
+			props: ['value'],
+      data: function() {
+        return {
+          options: [
+            "Manhole",
+            "Infiltration / Inflow",
+            "Connections",
+            "Other"
+          ]
+        }
+      },
+      computed: {
+        selected: {
+          get: function(value) {
+            return this.value
+          },
+          set: function(value) {
+            this.$emit('option', value);
+          }
+        }
+      }
+    }
+  },
+  methods: {
+    addCond: function(value) {
+			this.childSelected = value;
+			console.log(value);
+			if (value == "Manhole") {
+				this.manHole = true;
 			}
-		},
-		showPosition: function(position) {
-			this.lat = position.coords.latitude;
-			this.long = position.coords.longitude;
-		},
-		getDate: function() {
-			return this.date = Date();
-		},
-		imageOne: function() {
-			var x = this.$refs.inputOne;
-			this.getImageDetails(x);
-		},
-		imageTwo: function() {
-			var x = this.$refs.inputTwo;
-			this.getImageDetails(x)
-		},
-		getImageDetails: function(x) {
-			var array = this.images;
+    },
+    getLocation: function() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.showPosition);
+      } else {
+        console.log('Geolocation not supported');
+      }
+    },
+    showPosition: function(position) {
+      this.lat = position.coords.latitude;
+      this.long = position.coords.longitude;
+    },
+    getDate: function() {
+      return this.date = Date();
+    },
+    imageOne: function() {
+      var x = this.$refs.inputOne;
+      this.getImageDetails(x);
+    },
+    imageTwo: function() {
+      var x = this.$refs.inputTwo;
+      this.getImageDetails(x)
+    },
+    getImageDetails: function(x) {
+      var array = this.images;
 
-			if ('files' in x) {
-				if (x.files.length == 0) {
-					console.log("Select one or more files.");
-				} else {
+      if ('files' in x) {
+        if (x.files.length == 0) {
+          console.log("Select one or more files.");
+        } else {
 
-					for (var i = 0; i < x.files.length; i++) {
-						var file = x.files[i];
-						var image = new Image();
-						  var reader = new FileReader();
-						  var vm = this;
-						  reader.onload = (x) => {
-							vm.image = x.target.result;
-						  };
-						  reader.readAsDataURL(file);
-						if ('name' in file) {
-							array.push(file.name);
-						}
-						if ('webkitRelativePath' in file) {
-							array.push(file.webkitRelativePath);
-						}
-					}
-				}
-			}
-		},
-		onFileChange(e) {
-		  var files = e.target.files || e.dataTransfer.files;
-		  if (!files.length)
-			return;
-		  this.createImage(files[0]);
-		},
-		createImage(file) {
-		  var image = new Image();
-		  var reader = new FileReader();
-		  var vm = this;
+          for (var i = 0; i < x.files.length; i++) {
+            var file = x.files[i];
+            var image = new Image();
+            var reader = new FileReader();
+            var vm = this;
+            reader.onload = (x) => {
+              vm.image = x.target.result;
+            };
+            reader.readAsDataURL(file);
+            if ('name' in file) {
+              array.push(file.name);
+            }
+            if ('webkitRelativePath' in file) {
+              array.push(file.webkitRelativePath);
+            }
+          }
+        }
+      }
+    },
+    onFileChange(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length)
+        return;
+      this.createImage(files[0]);
+    },
+    createImage(file) {
+      var image = new Image();
+      var reader = new FileReader();
+      var vm = this;
 
-		  reader.onload = (e) => {
-			vm.image = e.target.result;
-		  };
-		  reader.readAsDataURL(file);
-		},
-		removeImage: function (e) {
-		  this.image = '';
-		},
-		changeItem: function changeItem(rowId, event) {
-		  this.selected2 = rowId + ", " + event.target.value;
-		  this.selected = event.target.value;
-		}
-	}
+      reader.onload = (e) => {
+        vm.image = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
+    removeImage: function(e) {
+      this.image = '';
+    },
+    changeItem: function changeItem(rowId, event) {
+      this.selected2 = rowId + ", " + event.target.value;
+      this.selected = event.target.value;
+    }
+  }
 })
